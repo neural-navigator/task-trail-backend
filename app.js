62a5cfb9893de7ec75206e4b39753ebd196a62b6
neural-navigator <iamsatyapati@gmail.com>
2024-07-06 16:13:42 +0530

    msignup process completed

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
 const express = require('express');
 const {dbConfig, serverPort} = require('./config/config');
+const mongoose = require('mongoose');
+const userRoutes = require('./middleware/userRoutes')
+const cors = require('cors');
+
+
+mongoose.connect(dbConfig.dbConnStr).then(
+    () => console.log("db connection successful!")
+).catch (
+    () => (console.log("connection failed"))
+);
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/api/v1', userRoutes);
+
+app.listen(serverPort, ()=>{
+    console.log(`server is running on ${serverPort}`);
+})
