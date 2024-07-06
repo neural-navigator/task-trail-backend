8ee813547fca2c7706e797ce48506dcbe2b09dc1
neural-navigator <iamsatyapati@gmail.com>
2024-07-06 17:44:43 +0530

    login controller added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,17 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 
 const createUser = async(req, res) => {
-
     const {name, email, password} = req.body;
-
     try {
-        let existingUser = await User.findOne({email});
+        const existingUser = await User.findOne({email});
         if (existingUser) {
             return res.status(400).json({error: "user already exists"});
         }
-
         const newUser = new User(req.body);
+        newUser.password = bcrypt.hash(newUser.password, 10);
         await newUser.save();
         res.status(201).json({message: "user created!"});
     } catch(error) {
@@ -18,6 +19,27 @@ const createUser = async(req, res) => {
     }
 };
 
+
+const checkUserStatus = async(req, res) => {
+    const {email, password} = req.body();
+    try{
+        const existingUser = await User.findOne({email});
+        if (!existingUser) {
+            return res.status(400).json({error: "user not found"})
+        }
+        const hashedPassword = bcrypt.hash(password, 10);
+        const existingPassword = existingUser.password;
+        const isMatch = bcrypt.compare(existingPassword, hashedPassword);
+        if (!isMatch) {
+            return res.status(400).json({error: "incorrect password!"})
+        }
+        const token = jwt.sign({userId: existingUser._id}, process.env.JWT_SECRET, {expiresIn: '1h'});
+        res.status(200).json({message: "login Succeed!", token: token})
+    } catch(error) {
+        res.status(500).json({error: `server error:: ${error}`})
+    }
+}
+
 module.exports = {
-    createUser
+    createUser, checkUserStatus
 };
